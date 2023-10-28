73c4e87a678bff1cc66075ddf2e09f58ed75244f
IanBUK <78267962+IanBUK@users.noreply.github.com>
2023-10-28 17:17:19 +0100

    add looped animation for Nathan model

--- main.gd
+++ main.gd
@@ -8,9 +8,21 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	prepareAnimations()
 	pass # Replace with function body.
 
+func prepareAnimations():
+	var anim =$Nathan/AnimationPlayer.get_animation("Armature|Take 001|BaseLayer")
+	anim.loop = true
+	
+	
+	#$Nathan/AnimationPlayer.play("Armature|Take 001|BaseLayer")
+	#$Nathan/AnimationPlayer
+	pass	
+
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
